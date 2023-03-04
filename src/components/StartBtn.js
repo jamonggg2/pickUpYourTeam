2ec030ae962d905360e753d3bf7b5999ac60de53
jujujamong <70519725+jamonggg2@users.noreply.github.com>
2023-03-05 02:26:46 +0900

    feat : 데이지 Ui 플러그인 추가

--- src/components/StartBtn.js
+++ src/components/StartBtn.js
@@ -1,6 +1,10 @@
-export default function StartBtn(){
+import '../fonts/font.css';
+export default function StartBtn({ idx, text, clickEvent }){
 
-    return(<div>
-        
-    </div>);
+    return(
+        <button className="btn btn-outline w-[250px] h-[50px] rounded-full text-white text-center m-[10px] p-[10px] text-[20px]"
+            style={{fontFamily:'neodgm'}}>
+            {text}
+        </button>
+    );
 };
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -5,22 +5,25 @@ import '../fonts/font.css';
 
 export default function StartPage(){
 
-    const [isShow, setIsShow] = useState(true);
+    const [isshow, setIsShow] = useState(true);
     const [isQuizShow, setIsQuizShow] = useState(false);
 
     const onClickStartBtn = () => {
         setIsShow(false);
         setIsQuizShow(true);
-    }
+    } 
 
     return(
-        <div className='w-full h-screen bg-red-700 flex-col justify-center items-center'>
+        <div className='w-full h-screen bg-red-700 flex-col justify-center items-center'
+            data-is-show={isshow}>
                <div className='text-center p-[20px]'>
                     <div className='font-sans text-white p-[30px] text-[40px] sm:text-[20px] font-bold' style={{fontFamily:'neodgm'}}>당신의 팀을 찾아보세요<br /> </div>
                     <div className='w-full bg-white flex justify-center rounded-md'><img src={f1Logo} className='w-[300px] sm:w-[150px] p-[10px] '/></div>
                     <div className='text-base text-white m-[10px]' style={{fontFamily:'neodgm'}}>2023 응원할 F1팀 고르기</div>
-                    <StartBtn type={true} text={"Pick a team to support"} onclick={onClickStartBtn} />
-                </div>
+                    <div className='flex justify-center items-center'>
+                        <StartBtn className='text-center' text={"Pick a team to support"} clickEvent={onClickStartBtn} />
+                    </div>
+                </div> 
         </div>
     );
 }
